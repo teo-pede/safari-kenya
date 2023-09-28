a42b6c57f84c226d3dbbed80b9276c0558fbcf40
Elia Pede <38972600+0EliaPede0@users.noreply.github.com>
2023-09-28 17:40:36 +0200

    Ciclo For almeno per brevità

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -7,59 +7,31 @@ import type { AccordionOptions, AccordionItem, AccordionInterface } from "flowbi
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent implements OnInit{
+export class FaqComponent implements OnInit {
 
-  ngOnInit(){
+  ngOnInit() {
     this.configAccordion()
   }
 
-  configAccordion(): void{
-    const accordionItems: AccordionItem[] = [
-      {
-          id: 'accordion-flush-heading-1',
-          triggerEl: <HTMLElement>document.getElementById('accordion-flush-heading-1'),
-          targetEl: <HTMLElement>document.getElementById('accordion-flush-body-1'),
-          active: true
-      },
-      {
-          id: 'accordion-flush-heading-2',
-          triggerEl: <HTMLElement>document.getElementById('accordion-flush-heading-2'),
-          targetEl: <HTMLElement>document.getElementById('accordion-flush-body-2'),
-          active: false
-      },
-      {
-          id: 'accordion-flush-heading-3',
-          triggerEl: <HTMLElement>document.getElementById('accordion-flush-heading-3'),
-          targetEl: <HTMLElement>document.getElementById('accordion-flush-body-3'),
-          active: false
-      },
-      {
-        id: 'accordion-flush-heading-4',
-        triggerEl: <HTMLElement>document.getElementById('accordion-flush-heading-4'),
-        targetEl: <HTMLElement>document.getElementById('accordion-flush-body-4'),
-        active: false
-      },
-      {
-        id: 'accordion-flush-heading-5',
-        triggerEl: <HTMLElement>document.getElementById('accordion-flush-heading-5'),
-        targetEl: <HTMLElement>document.getElementById('accordion-flush-body-5'),
-        active: false
-      },
-      {
-        id: 'accordion-flush-heading-6',
-        triggerEl: <HTMLElement>document.getElementById('accordion-flush-heading-6'),
-        targetEl: <HTMLElement>document.getElementById('accordion-flush-body-6'),
-        active: false
-      }
-    ];
-  
+  configAccordion(): void {
+    const accordionItems: AccordionItem[] = [];
+    for (let index = 0; index < 6; index++) {
+      accordionItems.push(
+        {
+          id: 'accordion-flush-heading-' + (index + 1),
+          triggerEl: <HTMLElement>document.getElementById('accordion-flush-heading-' + (index + 1)),
+          targetEl: <HTMLElement>document.getElementById('accordion-flush-body-' + (index + 1)),
+          active: index == 0 ? true : false
+        });
+    }
+
     const options: AccordionOptions = {
       alwaysOpen: false,
       activeClasses: 'text-gray-900 dark:text-whit',
       inactiveClasses: 'text-gray-700 dark:text-gray-400',
-      onOpen: () => {},
-      onClose: () => {},
-      onToggle: () => {},
+      onOpen: () => { },
+      onClose: () => { },
+      onToggle: () => { },
     };
 
     const accordion: AccordionInterface = new Accordion(accordionItems, options);
