f57214b3259d419681ee77ca62bd2fb8d6ef9a2f
MATTEO PEDERSOLI <Matteo.Pedersoli@ibm.com>
2023-10-05 16:43:22 +0200

    add bounce css and removed blue highlight on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,12 +45,26 @@ module.exports = {
           '0%': { opacity: 0, transform: 'translateY(+20px)' },
           '100%': { opacity: 1, transform: 'translateX(0)' }
         },
+        bounceInRight: {
+          '0%': { opacity: 0, transform: 'translateX(2000px)' },
+          '60%': { opacity: 1, transform: 'translateX(-30px)' },
+          '80%': { transform: 'translateX(10px)' },
+          '100%': { transform: 'translateX(0)' }
+        },
+        bounceInLeft: {
+          '0%': { opacity: 0, transform: 'translateX(-2000px)' },
+          '60%': { opacity: 1, transform: 'translateX(30px)' },
+          '80%': { transform: 'translateX(-10px)' },
+          '100%': { transform: 'translateX(0)' }
+        },
       },
       animation: {
         'fade-in-left': 'fadeInLeft 1s ease-in-out 1',
         'fade-in-right': 'fadeInRight 1s ease-in-out 1',
         'fade-in-up': 'fadeInUp 1s ease-in-out 1',
         'fade-in-down': 'fadeInDown 1s ease-in-out 1',
+        'bounce-in-right': 'bounceInRight 2s ease 1',
+        'bounce-in-left': 'bounceInLeft 2s ease 1'
       },
     },
   },
